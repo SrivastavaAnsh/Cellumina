c9c83e677e736200da8cc2431c35a236d7523aca
Ansh Srivastava <159410596+SrivastavaAnsh@users.noreply.github.com>
2026-04-05 20:35:37 +0530

    Initial Commit

---
+++ Cellumina/CelluminaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CelluminaApp.swift
+//  Cellumina
+//
+//  Created by Ansh Srivastava on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct CelluminaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
